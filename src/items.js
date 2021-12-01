d551c6b7d975a37120f9925bd7c76bb88262df98
Ben Read <ben.read@purplebricks.com>
2021-12-01 12:21:44 +0000

    init

---
+++ src/items.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, src: './yurt.jpg' },
+    { id: 2, src: 'https://picsum.photos/1240/900.jpg?random=2' },
+    { id: 3, src: 'https://picsum.photos/1800/900.jpg?random=3' },
+    { id: 4, src: 'https://picsum.photos/1540/900.jpg?random=4' },
+    { id: 5, src: 'https://picsum.photos/540/900.jpg?random=5' },
+    { id: 6, src: 'https://picsum.photos/1140/900.jpg?random=6' },
+    { id: 7, src: 'https://picsum.photos/1640/900.jpg?random=7' },
+    { id: 8, src: 'https://picsum.photos/1840/900.jpg?random=8' },
+    { id: 9, src: 'https://picsum.photos/3000/900.jpg?random=9' },
+]
---
+++ src/sliders.svelte
@@ -0,0 +1,72 @@
+<script>
+import items from './items.js';
+import { Slidy } from 'svelte-slidy';
+import Spinner from './Spinner.svelte'
+import SpinNew from './SpinNew.svelte'
+
+let name = 'Slidy',
+			index,
+			init = false
+	
+	$: init && (index = 2)
+	
+	const slidy = {
+		slides: items,
+		timeout: 1000,
+// 		index: 6,
+		wrap: {
+			id: 'slidy',
+			width: '100%',
+			height: '50vh',
+			padding: '0',
+			align: 'middle',
+			alignmargin: 0,
+		},
+		slide: {
+			gap: 0,
+			width: '50vw',
+			height: '100%',
+			backimg: false,
+			imgsrckey: 'src',
+			objectfit: 'cover',
+			overflow: 'hidden'
+		},
+		controls: {
+			dots: true,
+			dotsnum: true,
+			dotsarrow: true,
+			dotspure: false,
+			arrows: true,
+			keys: true,
+			drag: true,
+			wheel: true,
+		},
+		options: {
+			axis: 'x',
+			loop: true,
+			duration: 350,
+		}
+	}
+</script>
+
+<h1>Hello <span>{name}</span>!</h1>
+<Slidy {...slidy} bind:index bind:init>
+	<span slot="loader">
+		<SpinNew />
+	</span>
+</Slidy>
+
+<style>
+	:global(#slidy span) {
+		left: 50%;
+		transform: translateX(-50%)
+	}
+	@media screen and (min-width: 900px) {
+		:global(#slidy .slidy-ul li) {width: 33vw;}
+	}
+	@media screen and (max-width: 425px) {
+		:global(#slidy .slidy-ul li) {width: 100vw;}
+	}
+	h1 span {color: red}
+	h1 {text-align: center}
+</style>
